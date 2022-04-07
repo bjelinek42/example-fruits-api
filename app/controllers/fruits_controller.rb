4ddd7eaf31010dedebac38ceedf6ab2139f03ac2
ben <bjelinek42@gmail.com>
2022-04-07 13:13:30 -0600

    create and index front and back

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -3,4 +3,13 @@ class FruitsController < ApplicationController
     fruits = Fruit.all
     render json: fruits
   end
+
+  def create
+    fruit = Fruit.new(
+      name: params[:name],
+      image: params[:image],
+      quality: params[:quality]
+    )
+    fruit.save
+  end
 end
--- example-fruits-api-frontend/src/views/HomeView.vue
+++ example-fruits-api-frontend/src/views/HomeView.vue
@@ -1,18 +1,54 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to Fruits!",
+      fruits: [],
+      newFruitParams: {}
+    };
+  },
+  created: function () {
+    this.indexFruits()
+  },
+  methods: {
+    indexFruits: function () {
+      axios.get(`/fruits`).then(response => {
+        console.log("index fruits", response)
+        this.fruits = response.data
+      })
+    },
+    createFruit: function () {
+      axios.post(`/fruits`, this.newFruitParams).then(response => {
+        console.log('creating fruit', response)
+        this.fruits.push(response.data)
+      })
+    }
+  },
+};
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <p>Create New Fruit</p>
+    <p>
+      Name:
+      <input type="text" v-model="newFruitParams.name" />
+    </p>
+    <p>
+      Image:
+      <input type="text" v-model="newFruitParams.image" />
+    </p>
+    <p>
+      Quality:
+      <input type="text" v-model="newFruitParams.quality" />
+    </p>
+    <button v-on:click="createFruit">Create!</button>
+    <div v-for="fruit in fruits" v-bind:key="fruit.id">
+      <p>{{ fruit.name }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style></style>
